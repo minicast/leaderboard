3357446ceb591c2654dc5458173667f6dd507116
minicast <minicast@gmx.com>
2015-12-21 16:33:39 +0100

    initial commit

--- tests/gagarin/tests.js
+++ tests/gagarin/tests.js
@@ -1,11 +1,22 @@
-describe('My first Gagarin test suite', function () {
-  var server = meteor();
-  it('should just work', function () {
-    return server.execute(function () {
-      console.log('I am alive!');
-    });
+describe("simple gagarin test", function() {
+
+  it("should be passed", function() {
+
+    expect(10).to.equal(10);
+
   });
+
 });
+
+// describe('My first Gagarin test suite', function () {
+//   var server = meteor();
+//   var client = browser(server);
+//   it('should just work', function () {
+//     return server.execute(function () {
+//       console.log('I am alive!');
+//     });
+//   });
+// });
 
 // deployment:
 //   production:
